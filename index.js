0c61562931f41b3a173c3ed73e723f1db0d03da7
Ondřej Tůma <ondr@centrum.cz>
2021-03-08 18:26:54 +0100

    master: getting current branch and deploy type

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-(async () => {
-  const simpleGit = require('simple-git')
-  const inquirer = require('inquirer')
+const simpleGit = require('simple-git')
+const inquirer = require('inquirer')
 
-  const workingDir = process.argv[2] || './'
+const workingDir = process.argv[2] || './'
 
-  const git = simpleGit(workingDir)
+const git = simpleGit(workingDir)
 
+const typesOfDeploy = {
+  type: 'list',
+  name: 'deployType',
+  message: 'Type of Deploy?',
+  choices: [
+    { name: 'Feature branch', value: 'f' },
+    { name: 'Beta', value: 'b' },
+    { name: 'Production', value: 'p' },
+  ]
+}
+
+async function run() {
   const questions = [
-    {
-      type: 'list',
-      name: 'deployType',
-      message: 'Type of Deploy?',
-      choices: [
-        { name: 'Feature branch', value: 'f' },
-        { name: 'Beta', value: 'b' },
-        { name: 'Production', value: 'p' },
-      ]
-    }
+    typesOfDeploy,
   ]
 
   try {
@@ -29,4 +31,6 @@
   } catch (e) {
     console.error(e.message)
   }
-})()
+}
+
+run()
